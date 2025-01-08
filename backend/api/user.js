d4dd31db1720ab9728f9673a116c95351a110bd9
jason2698 <jjason2698@yahoo.com>
2025-01-08 23:25:00 +0530

    signup functionality completed and tested in postman

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
+//DB details
+const User = require('./../models/user');
+
+//password handler
+const bcrypt = require('bcrypt');
+
 //signup
 router.post('/signup', (req,res)=>{
+    let{name, email, password, dateOfBirth} = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
 
+    User.find({email}).then(result=>{
+        if(result.length){
+            res.json({
+                status : "Failed",
+                message : "User with the provided email already exists"
+            })
+        }else{
+            //try to create new user
+
+            //password handling
+            const salRounds = 10;
+            bcrypt.hash(password, salRounds).then(hashedPass=>{
+                const newUser = new User({
+                    name,
+                    email,
+                    password: hashedPass,
+                    dateOfBirth
+                });
+                newUser.save().then(result=>{
+                    res.json({
+                        status: "Success",
+                        message: "Signup successful",
+                        data: result
+                    })
+                }).catch(err=>{
+                    res.json({
+                        status : "Failed",
+                        message : "An error occured while checking for existing user!"
+                    })
+                })
+            })
+        }
+    }).catch(err=>{
+        console.log(err);
+        res.json({
+            status : "Failed",
+            message : "An error occured while checking for existing user!"
+        })
+    })
 })
 
 //Signin
