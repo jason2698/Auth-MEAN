589117d81df51e8d16f1ab3cf3761d621dc773f9
jason2698 <jjason2698@yahoo.com>
2025-01-28 23:52:28 +0530

    Mail option code added

--- backend/api/user.js
+++ backend/api/user.js
@@ -16,6 +16,29 @@ const nodeMailer = require('nodemailer');
 //unique string
 const {v4: uuidv4} = require("uuid")
 
+//ENV variables
+require("dotenv").config();
+
+// nodemailer stuff
+let transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.AUTH_PASS
+    }
+})
+
+//nodemailer testing
+transporter.verify((error, success)=>{
+    console.log("Entry")
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Ready for messages");
+        console.log(success); 
+    }
+})
+
 //signup
 router.post('/signup', (req,res)=>{
     let{name, email, password, dateOfBirth} = req.body;
@@ -40,14 +63,17 @@ router.post('/signup', (req,res)=>{
                     name,
                     email,
                     password: hashedPass,
-                    dateOfBirth
+                    dateOfBirth,
+                    verified: false
                 });
                 newUser.save().then(result=>{
-                    res.json({
-                        status: "Success",
-                        message: "Signup successful",
-                        data: result
-                    })
+                    // res.json({
+                    //     status: "Success",
+                    //     message: "Signup successful",
+                    //     data: result
+                    // })
+
+                    sendVerificationEmail(result, res);
                 }).catch(err=>{
                     res.json({
                         status : "Failed",
@@ -64,6 +90,70 @@ router.post('/signup', (req,res)=>{
         })
     })
 })
+
+//send verification email
+const sendVerificationEmail = ({_id, email}, res) => {
+    //url to be used in the email
+    const currentUrl = "https://localhost:5000/";
+
+    const uniqueString = uuidv4() + _id;
+
+    //mail options
+    const mailOptions = {
+        from : process.env.AUTH_EMAIL,
+        to: email,
+        subject : "verify your email",
+        html: `<p>Verify your email address to complete the signup and login into your account.</p>
+        <p>This link <b>expires in 6 hours</b>.</p>
+        <p>Press <a href=${currentUrl +"user/verify/" +_id + "/"+ uniqueString}
+        >here</a>to procced.</p>`,
+    }
+
+    //hash the unique string
+    const salRounds = 10;
+    bcrypt
+    .hash(uniqueString, salRounds)
+    .then((hashedUniqueString)=>{
+        const newVerification = new UserVerification({
+            userId: _id,
+            uniqueString: hashedUniqueString,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 21600000 
+        });
+
+        newVerification
+        .save()
+        .then(()=>{
+            transporter
+            .sendMail(mailOptions)
+            .then(()=>{
+                res.json({
+                    status: "PENDING",
+                    message: "Verification email sent"
+                })
+            })
+            .catch((error)=>{
+                console.log(error)
+                res.json({
+                    status: "FAILED",
+                    message: "Verification email failed"
+                })
+            })
+        })
+        .catch(()=>{
+            res.json({
+                status: "FAILED",
+                message: "Couldn't save email verification data!"
+            })
+        })
+    })
+    .catch(()=>{
+        res.json({
+            status: "FAILED",
+            message: "An error occured while hashing emial data!"
+        })
+    })
+}
 
 //Signin
 router.post('/signin', (req, res)=>{
